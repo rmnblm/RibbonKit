5a5308aabc660bb47c238b87bb16c00d24603fe0
Roman Blum <roman.blum1@swisscom.com>
2023-10-27 17:57:36 +0200

    Added SwipeActionsConfigurationProvider to RibbonListCollectionViewListConfiguration

--- Sources/RibbonKit/List/RibbonListCollectionViewListConfiguration.swift
+++ Sources/RibbonKit/List/RibbonListCollectionViewListConfiguration.swift
@@ -2,15 +2,33 @@
 
 import UIKit
 
-public struct RibbonListCollectionViewListConfiguration: Hashable {
+public struct RibbonListCollectionViewListConfiguration {
 
-    public var appearance: UICollectionLayoutListConfiguration.Appearance
-    public var backgroundColor: UIColor?
+    public let id = UUID()
+    public let appearance: UICollectionLayoutListConfiguration.Appearance
+    public let backgroundColor: UIColor?
 
     #if os(iOS)
-    public var showsSeparators: Bool = true
+    public let showsSeparators: Bool
+    public let leadingSwipeActionsConfiguration: UICollectionLayoutListConfiguration.SwipeActionsConfigurationProvider?
+    public let trailingSwipeActionsConfiguration: UICollectionLayoutListConfiguration.SwipeActionsConfigurationProvider?
     #endif
 
+    #if os(iOS)
+    public init(
+        appearance: UICollectionLayoutListConfiguration.Appearance,
+        backgroundColor: UIColor? = nil,
+        showsSeparators: Bool = true,
+        leadingSwipeActionsConfiguration: UICollectionLayoutListConfiguration.SwipeActionsConfigurationProvider? = nil,
+        trailingSwipeActionsConfiguration: UICollectionLayoutListConfiguration.SwipeActionsConfigurationProvider? = nil
+    ) {
+        self.appearance = appearance
+        self.backgroundColor = backgroundColor
+        self.showsSeparators = showsSeparators
+        self.leadingSwipeActionsConfiguration = leadingSwipeActionsConfiguration
+        self.trailingSwipeActionsConfiguration = trailingSwipeActionsConfiguration
+    }
+    #else
     public init(
         appearance: UICollectionLayoutListConfiguration.Appearance,
         backgroundColor: UIColor? = nil
@@ -18,13 +36,26 @@ public struct RibbonListCollectionViewListConfiguration: Hashable {
         self.appearance = appearance
         self.backgroundColor = backgroundColor
     }
+    #endif
 
     func asCollectionLayoutListConfiguration() -> UICollectionLayoutListConfiguration {
         var config = UICollectionLayoutListConfiguration(appearance: appearance)
-        config.backgroundColor = backgroundColor
         #if os(iOS)
         config.showsSeparators = showsSeparators
+        config.leadingSwipeActionsConfigurationProvider = leadingSwipeActionsConfiguration
+        config.trailingSwipeActionsConfigurationProvider = trailingSwipeActionsConfiguration
         #endif
+        config.backgroundColor = backgroundColor
         return config
+    }
+}
+
+extension RibbonListCollectionViewListConfiguration: Hashable {
+    public static func == (lhs: RibbonListCollectionViewListConfiguration, rhs: RibbonListCollectionViewListConfiguration) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
